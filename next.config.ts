11f59a33d74afcd8689f00969e731053dfd6665a
Vinicius Pinheiro <viniropi@gmail.com>
2026-02-22 15:52:48 -0300

    refactor: remove experimental serverActions configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: process.env.CODESPACES
-        ? [
-            `${process.env.CODESPACE_NAME}-3000.app.github.dev`,
-            "localhost:3000",
-          ]
-        : ["localhost:3000"],
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;

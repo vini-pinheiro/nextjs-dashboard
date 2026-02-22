f375cf6126d2cf5b65dd3b977bd9348e974cfe18
Vinicius Pinheiro <viniropi@gmail.com>
2026-02-22 15:41:05 -0300

    feat: add NotFound and Error components for invoice handling; enhance error handling in actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,12 +25,15 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
-
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-
+  } catch (error) {
+    console.error("Failed to create invoice: ", error);
+    throw new Error("Faile to create invoice.");
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
@@ -43,18 +46,28 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
 
   const amountInCents = amount * 100;
-
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-
+  } catch (error) {
+    console.error("Failed to update invoice: ", error);
+    throw new Error("Faile to update invoice.");
+  }
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  throw new Error("Failed to Delete Invoice");
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    console.error("Failed to delete invoice: ", error);
+    throw new Error("Faile to delete invoice.");
+  }
   revalidatePath("/dashboard/invoices");
 }
